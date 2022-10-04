7b8a104961a5a96815ec4c206393c7df1994e43c
Sandhya Kalakada <happylearning2022@gmail.com>
2022-10-03 20:43:15 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'profile';
+  user:User = {
+    name: "Dhoni",
+    age:25,
+    favoriteColor: "black",
+    birthday:new Date(),
+    caffeine: "Water"
+  };
+
+  displayEdit:boolean = false;
+
+  toggelEdit():void{
+    this.displayEdit = !this.displayEdit;
+  }
 }
---
+++ src/app/user.ts
@@ -0,0 +1,8 @@
+export interface User {
+    name: string;
+    age: number;
+    favoriteColor:string;
+    birthday:Date;
+    caffeine:string;
+}
+
